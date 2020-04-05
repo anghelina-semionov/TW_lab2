54dfcdd4d6966d9db6b91a00c72551405c920490
anghelina-semionov <62342202+anghelina-semionov@users.noreply.github.com>
2020-04-06 00:37:03 +0300

    Create HomeController.cs

---
+++ WebApplication_mvc_19_03/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_mvc_19_03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Stream()
+        {
+            return View();
+        }
+
+        public ActionResult Library()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Recoverpw()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Favourite()
+        {
+            return View();
+        }
+        public ActionResult Following()
+        {
+            return View();
+        }
+        public ActionResult Parameters()
+        {
+            return View();
+        }
+    }
+}
